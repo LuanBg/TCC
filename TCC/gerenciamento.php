b51f1bd814b65b16add9ad927482ce41cd2c53e3
Samir carlos <samir_silva20@outlook.com>
2025-03-23 22:55:41 -0300

    att

---
+++ TCC/gerenciamento.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Configurações / Bomfim Contabilidade</title>
+    <link rel="stylesheet" href="css/style7.css">
+</head>
+<body>
+    <div class="user-info">
+    <img src="images/use3-removebg-preview.png" alt="User">
+    <h3>USUÁRIO</h3>
+
+    <div class="actions">
+    <div class="back">
+        <img src="images/voltar2-removebg-preview.png" alt="Back">
+        <a href="home.php">Voltar</a>
+    </div>
+    <div class="logout">
+        <img src="images/desconectar-removebg-preview.png" alt="Logout">
+        <a href="index.html">Desconectar</a>
+    </div>
+</div>
+    </div>
+
+<div class="content">
+    
+    <div class="logo">
+        <img src="images/logo.png" alt="Logo">
+    </div>
+
+    <div class="form-group">
+        <h2>Cadastor de Usuário</h2>
+        <input type="email" placeholder="Email">
+        <input type="text" placeholder="Senha">
+        
+        <button onclick="window.location.href='sobre.php'">Cadastra</button>
+   
+
+    </div>
+<script src="js/script.js"></script> 
+</body>
+</html>
