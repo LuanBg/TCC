473a0dc7900f9f06e719691274d1829fdbced5a9
BrenoAraujo007 <breno.a.santos6@ba.estudante.senai.br>
2025-04-06 12:10:33 -0300

    conexaofeita

--- conexao.php
+++ conexao.php
@@ -1,12 +1,19 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "cimatec"; // Se tiver senha, adicione aqui
-$database = "SistemaNotas";
+$host = 'localhost';
+$port = '3307'; // Porta do seu MySQL
+$db   = 'SistemaNotas';
+$user = 'root';
+$pass = 'breno'; // ou a senha correta, se você definiu uma
+$charset = 'utf8mb4';
 
-$conn = new mysqli($host, $user, $password, $database);
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+try {
+    $conn = new PDO($dsn, $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (\PDOException $e) {
+    echo "Conexão falhou: " . $e->getMessage();
+    $conn = null; // define como null para evitar erro no index.php
 }
 ?>
+
---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home / Bomfim Contabilidade</title>
+    <link rel="stylesheet" href="css/style4.css">
+</head>
+<body>
+    <div class="user-info">
+    <img src="images/use3-removebg-preview.png" alt="User">
+    <h3>USUÁRIO</h3>
+
+    <div class="logout">
+        <img src="images/desconectar-removebg-preview.png" alt="Logout Icon">
+        <a href="index.html">Desconectar</a>
+    </div>
+    </div>
+
+    <div class="content">
+    
+    <div class="form-group">
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo">
+        </div>
+
+        <button type="button" name="sobrenome">Download de Notas</button>
+        <button type="button" name="email">Cadastro</button>
+        <button onclick="window.location.href='configuracoes.php'">Configurações</button>
+        <button type="button" name="endereco">Histórico</button>
+    </div>
+
+    </div>
+<script src="js/script.js"></script> 
+</body>
+</html>
