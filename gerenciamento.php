95d3dc3828a69f734c98d3f5acabde5a85b82906
Samir carlos <samir_silva20@outlook.com>
2025-04-27 08:14:32 -0300

    att

--- gerenciamento.php
+++ gerenciamento.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Configurações / Bomfim Contabilidade</title>
     <link rel="stylesheet" href="css/style5.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -22,8 +23,12 @@
         </div>
 
         <div class="form-group">
+
             <h2>Gerenciamento</h2>
-            <button onclick="window.location.href='clear'">Excluir</button>
+            <input type="text" name="pesquisa" id="pesquisa" require>
+            <h2>Usuário</h2>
+            <input type="image" src="images/icon_perfil.png" alt="perfil">
+            <button onclick="window.location.href='gerenciamento.php'">Excluir</button>
             <button onclick="window.location.href=''">Editar</button>
         </div>
 
@@ -31,7 +36,7 @@
             function excluir() {
                 if (confirm("Tem certeza que deseja excluir os dados?")) {
                     // Substitua 'delete.php' pelo caminho real do seu script de exclusão
-                    window.location.href = 'delete.php';
+                    window.location.href = 'gerenciamento.php';
                 }
             }
         </script>
